465f15057e854a04bcf01c405e933e4c84d6607e
Stegosavr <ilya18092000@gmail.com>
2024-05-16 02:13:47 +0500

    fixed back prop

--- PerceptronPP/PerceptronPP.Core/Tools/Weights/Provider/RandomWeightsProvider.cs
+++ PerceptronPP/PerceptronPP.Core/Tools/Weights/Provider/RandomWeightsProvider.cs
@@ -6,11 +6,22 @@ public class RandomWeightsProvider : IWeightsProvider
 
 	public RandomWeightsProvider(int layer)
 	{
-		_distribution = (1 / Math.Sqrt(layer));
+		//_distribution = (1 / Math.Sqrt(layer));
+
+		_distribution = 1;
 	}
 	
 	public double GetWeight(int _, int __)
 	{
-		return new Random().NextDouble() * _distribution * 50 - _distribution * 25;
+		//return new Random().NextDouble() * _distribution * 2 - _distribution *1;
+
+		Random rand = new Random(); //reuse this if you are generating many
+		double u1 = 1.0 - rand.NextDouble(); //uniform(0,1] random doubles
+		double u2 = 1.0 - rand.NextDouble();
+		double randStdNormal = Math.Sqrt(-2.0 * Math.Log(u1)) *
+					 Math.Sin(2.0 * Math.PI * u2); //random normal(0,1)
+		double randNormal =
+					 0 + _distribution * randStdNormal; //random normal(mean,stdDev^2)
+		return randNormal;
 	}
 }
--- PerceptronPP/PerceptronPP/Program.cs
+++ PerceptronPP/PerceptronPP/Program.cs
@@ -9,11 +9,26 @@ namespace PerceptronPP;
 
 internal static class Program
 {
-	public static void Main()
-	{
-		//using (var rider = new IdxReader(@"..\..\..\..\..\Datasets\MNIST\t10k-labels.idx1-ubyte")) { var f = rider.ReadVector(); }
+    private static void Main2()
+    {
+        var network = new Network(new AtanComputable(), 3, 3);//3,4,3
+        network
+            //.SetWeights(new ConstantWeightFactory(new[] { new[,] { { 0.5 }, { 2.0 } }, new[,] { { 1, 1.0 } } }))
+            .SetWeights(new ConstantWeightFactory(new[] { new[,] { { 1, 1, 1 }, { 1, 1, 1 }, { 1, 1, 1d } } }))
+            .SetBiases(new BiasConstantProvider(0));
 
-			var network = new Network(new SigmoidComputable(), 784, 16, 16,10);//3,4,3
+        var output = network.Compute(new[] { 1, 1, 1d });
+        network.CalculateCost(output, new[] { 1, 1, 1d });
+        network.BackPropagate(output, new[] { 1, 1, 1d });
+        network.GradientDescend(0.05);
+    }
+
+    public static void Main()
+    {
+        //Main2();
+        //return;
+
+        var network = new Network(new AtanComputable(), 784, 30, 30,10);//3,4,3
 		network
 			//.SetWeights(new ConstantWeightFactory(new[] { new[,] { { 0.5 }, { 2.0 } }, new[,] { { 1, 1.0 } } }))
 			.SetWeights(new RandomWeightsFactory(network.GetNeuronCount))
@@ -44,20 +59,37 @@ internal static class Program
         var labeledImages = labels.Select((label,i) => Tuple.Create(label,images[i]));
         var sorted = labeledImages.OrderBy(x => x.Item1).Select(tuple => Tuple.Create(Learning.IntToExpectedOutputArray(tuple.Item1),tuple.Item2));
         var dict = labeledImages.ToLookup(e => e.Item1, e => e.Item2);
-        var hui = dict.ToDictionary(e=>e.Key,e=>e.Skip(1).ToArray());
+        var Dict = dict.ToDictionary(e=>e.Key,e=>e.Skip(1).ToArray());
         var input1 = sorted.Select(x => x.Item1).ToArray();
         var input2 = sorted.Select(x => x.Item2).ToArray();
 
+        int n;
+
+        Learning.Learn(network, 4, 20, images, labels);
+        //for (n = 0; n < 1; n++) LERN(network, new[] { Dict[0].Take(6000).ToArray() }, new[] { 0 });
+        //for (n = 0; n < 1; n++) LERN(network, new[] { Dict[0].Take(6000).ToArray(), (Dict[1].Take(5000)).ToArray() }, new[] { 0,1 });
+        //for (n = 0; n < 1; n++) LERN(network, new[] { Dict[0].Take(5000).ToArray(), (Dict[1].Take(5000)).ToArray(), (Dict[2].Take(5000)).ToArray() }, new[] { 0,1,2 });
+        //for (n = 0; n < 1; n++) LERN(network, new[] { Dict[0].Take(5000).ToArray(), (Dict[1].Take(5000)).ToArray(), (Dict[2].Take(5000)).ToArray(), (Dict[3].Take(5000)).ToArray() }, new[] { 0,1,2,3 });
+        //for (n = 0; n < 1; n++) LERN(network, new[] { Dict[0].Take(5000).ToArray(), (Dict[1].Take(5000)).ToArray(), (Dict[2].Take(5000)).ToArray(), (Dict[3].Take(5000)).ToArray(), (Dict[4].Take(5000)).ToArray() }, new[] { 0,1,2,3,4 });
+        //for (n = 0; n < 4; n++) LERN(network, new[] { Dict[0].Take(2500).ToArray(), (Dict[1].Take(2500)).ToArray(), (Dict[2].Take(2500)).ToArray(), (Dict[3].Take(2500)).ToArray(), (Dict[4].Take(2500)).ToArray(), (Dict[5].Take(2500)).ToArray() }, new[] { 0, 1, 2, 3, 4 ,5});
+        //for (n = 0; n < 5; n++) LERN(network, new[] { Dict[0].Take(2500).ToArray(), (Dict[1].Take(2500)).ToArray(), (Dict[2].Take(2500)).ToArray(), (Dict[3].Take(2500)).ToArray(), (Dict[4].Take(2500)).ToArray(), (Dict[5].Take(2500)).ToArray(), (Dict[6].Take(2500)).ToArray() }, new[] { 0, 1, 2, 3, 4, 5,6 });
+        //for (n = 0; n < 5; n++) LERN(network, new[] { Dict[0].Take(2500).ToArray(), (Dict[1].Take(2500)).ToArray(), (Dict[2].Take(2500)).ToArray(), (Dict[3].Take(2500)).ToArray(), (Dict[4].Take(2500)).ToArray(), (Dict[5].Take(2500)).ToArray(), (Dict[6].Take(2500)).ToArray(), (Dict[7].Take(2500)).ToArray() }, new[] { 0, 1, 2, 3, 4, 5, 6,7 });
+        //for (n = 0; n < 6; n++) LERN(network, new[] { Dict[0].Take(2500).ToArray(), (Dict[1].Take(2500)).ToArray(), (Dict[2].Take(2500)).ToArray(), (Dict[3].Take(2500)).ToArray(), (Dict[4].Take(2500)).ToArray(), (Dict[5].Take(2500)).ToArray(), (Dict[6].Take(2500)).ToArray(), (Dict[7].Take(2500)).ToArray(), (Dict[8].Take(2500)).ToArray() }, new[] { 0, 1, 2, 3, 4, 5, 6, 7, 8 });
+        //for (n = 0; n < 6; n++) LERN(network, new[] { Dict[0].Take(2500).ToArray(), (Dict[1].Take(2500)).ToArray(), (Dict[2].Take(2500)).ToArray(), (Dict[3].Take(2500)).ToArray(), (Dict[4].Take(2500)).ToArray(), (Dict[5].Take(2500)).ToArray(), (Dict[6].Take(2500)).ToArray(), (Dict[7].Take(2500)).ToArray(), (Dict[8].Take(2500)).ToArray(), (Dict[9].Take(2500)).ToArray() }, new[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 });
 
 
-        Learning.Learn(network, 12, 1, input2, input1);
+
+
+
+
+
 
         using var mnist3 = new IdxReader(@"..\..\..\..\Datasets\MNIST\t10k-images.idx3-ubyte");
         using var mnist4 = new IdxReader(@"..\..\..\..\Datasets\MNIST\t10k-labels.idx1-ubyte");
 
         var images2 = GetSample(mnist3).ToArray();
-        var labels2 = GetLabel(mnist4).Select(e=>Learning.IntToExpectedOutputArray(e)).ToArray();
-
+        var labels2 = GetLabel(mnist4).ToArray();
+        Console.Clear();
         Learning.Test(network,images2,labels2);
 
 
@@ -88,6 +120,28 @@ internal static class Program
         //	network.BackPropagate(result, expected);
         //	network.GradientDescend(0.01);
         //}
+    }
+
+    public static void LERN(Network network,double[][][] trainData,int[] expectedOutput)
+    {
+        for (int i = 0; i < 5000; i++)
+        {
+            for (int num = 0; num < trainData.Length; num++)
+            {
+                Learning.Iterate(network, trainData[num][i], expectedOutput[num]);
+
+            }
+            if ((i + 1) % 5 == 0)
+            {
+                //Thread.Sleep();
+                network.GradientDescend(4);
+                Console.WriteLine(network.GetCost());
+                Console.WriteLine(trainData.Length);
+                var (_, top) = Console.GetCursorPosition();
+                Console.SetCursorPosition(0, top - 2);
+                network.ResetCost();
+            }
+        }
     }
 }
 
