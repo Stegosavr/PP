4b704a40fd94c1b7966094c7380c5c46a4dc9c2d
Stegosavr <ilya18092000@gmail.com>
2024-05-18 14:46:53 +0500

    Optimizer

--- PerceptronPP/PerceptronPP.Core/GradientDescentData.cs
+++ PerceptronPP/PerceptronPP.Core/GradientDescentData.cs
@@ -1,6 +1,15 @@
-﻿namespace PerceptronPP.Core.Tools.Computable
+﻿using MathNet.Numerics.LinearAlgebra;
+namespace PerceptronPP.Core
 {
-    public class DradientDescentData
+    public class GradientDescentData
     {
+        public readonly Matrix<double> PreviousWeightsGradient;
+        public readonly Matrix<double> PreviousBiasesGradient;
+
+        public GradientDescentData(Matrix<double> prevWeightsGrad, Matrix<double> prevBiasesGrad)
+        {
+            PreviousWeightsGradient = prevWeightsGrad;
+            PreviousBiasesGradient = prevBiasesGrad;
+        }
     }
 }
